19b3dd9c2432410168b96ff04f3e78b3d33022e9
TARANAMURADLI <tarana.muradli@gmail.com>
2023-01-29 11:42:31 +0400

    sened

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let click=document.querySelector('click');
+
+
+
+let Home=document.querySelector('.Home');
+
+
+Home.addEventListener('click',()=>Home.style.backgroundColor="yellow")
+if (click) {
+    Home.color.yellow
+}else{without.Click
+    Home.color.black
+}
+
+let Page=document.querySelector('.Page');
+
+
+Page.addEventListener('click',()=>Page.style.backgroundColor="yellow")
+
+let About=document.querySelector('.About');
+
+
+About.addEventListener('click',()=>About.style.backgroundColor="yellow")
+
+let Contact=document.querySelector('.Contact');
+
+
+Contact.addEventListener('click',()=>Contact.style.backgroundColor="yellow")
+
